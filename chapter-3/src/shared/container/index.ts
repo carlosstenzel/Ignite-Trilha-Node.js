888699c5d58756277bccb1435b8216c32768ba5e
Carlos Stenzel <carlosstenzel@hotmail.com>
2021-06-12 17:18:10 -0300

    finalizando

---
+++ chapter-3/src/shared/container/index.ts
@@ -0,0 +1,26 @@
+import { container } from 'tsyringe';
+
+import { UsersRepository } from '../../modules/accounts/repositories/implementations/UsersRepository';
+import { IUsersRepository } from '../../modules/accounts/repositories/IUserRepository';
+
+import { ICategoriesRepository } from '../../modules/cars/repositories/ICategoriesRepository';
+import { CategoriesRepository } from '../../modules/cars/repositories/implementations/CategoriesRepository';
+
+import { SpecificationsRepository } from '../../modules/cars/repositories/implementations/SpecificiationsRepository';
+import { ISpecificationsRepository } from '../../modules/cars/repositories/ISpecificationsRepository';
+
+
+container.registerSingleton<ICategoriesRepository>(
+    "CategoriesRepository",
+    CategoriesRepository
+);
+
+container.registerSingleton<ISpecificationsRepository>(
+    "SpecificationsRepository",
+    SpecificationsRepository
+);
+
+container.registerSingleton<IUsersRepository>(
+    "UsersRepository",
+    UsersRepository
+);
