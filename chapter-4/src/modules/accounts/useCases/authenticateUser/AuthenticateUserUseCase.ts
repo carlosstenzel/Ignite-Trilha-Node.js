7858bbf846a9d7ca191df68d3259d9ac0b9ad190
Carlos Stenzel <carlosstenzel@hotmail.com>
2021-06-14 13:39:52 -0300

    Start config

---
+++ chapter-4/src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.ts
@@ -0,0 +1,61 @@
+import {compare} from 'bcryptjs';
+import { inject, injectable } from "tsyringe";
+import { sign } from 'jsonwebtoken';
+
+import { IUsersRepository } from "../../repositories/IUserRepository";
+import { AppError } from '../../../../errors/AppError';
+
+
+interface IRequest {
+    email: string;
+    password: string;
+}
+
+interface IResponse {
+    user: {
+        name: string;
+        email: string;
+    };
+    token: string;
+}
+
+@injectable()
+class AuthenticateUserUseCase {
+
+    constructor(
+        @inject("UsersRepository")
+        private usersRepository: IUsersRepository
+    ) {}
+
+    async execute({email, password}: IRequest) : Promise<IResponse> {
+        const user = await this.usersRepository.findByEmail(email);
+
+        if(!user) {
+            throw new AppError("Email or password incorrect");
+        }
+
+        const passwordMatch = await compare(password, user.password);
+
+        if(!passwordMatch) {
+            throw new AppError("Email or password incorrect");
+        }
+
+        const token = sign({}, "ignite3434343434", {
+            subject: user.id,
+            expiresIn: "1d",
+        });
+
+        const tokenReturn : IResponse = {
+            token,
+            user: {
+                name: user.name,
+                email: user.email
+            }
+        };
+
+        return tokenReturn;
+    }
+
+}
+
+export { AuthenticateUserUseCase }
