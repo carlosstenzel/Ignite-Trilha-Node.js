5bd4fdc9d6d3ca34147deafbe7646ecef78a5154
Carlos Stenzel <carlosstenzel@hotmail.com>
2021-07-13 20:20:54 -0300

    Caso de uso do cadastro de images do carro

--- chapter-4/src/shared/container/index.ts
+++ chapter-4/src/shared/container/index.ts
@@ -1,30 +1,36 @@
-import { container } from 'tsyringe';
+import { container } from "tsyringe";
 
-import { UsersRepository } from '@modules/accounts/infra/typeorm/repositories/UsersRepository';
-import { IUsersRepository } from '../../modules/accounts/repositories/IUserRepository';
+import { UsersRepository } from "@modules/accounts/infra/typeorm/repositories/UsersRepository";
+import { IUsersRepository } from "../../modules/accounts/repositories/IUserRepository";
 
-import { ICategoriesRepository } from '../../modules/cars/repositories/ICategoriesRepository';
-import { CategoriesRepository } from '../../modules/cars/infra/typeorm/repositories/CategoriesRepository';
+import { ICategoriesRepository } from "../../modules/cars/repositories/ICategoriesRepository";
+import { CategoriesRepository } from "../../modules/cars/infra/typeorm/repositories/CategoriesRepository";
 
-import { SpecificationsRepository } from '../../modules/cars/infra/typeorm/repositories/SpecificiationsRepository';
-import { ISpecificationsRepository } from '../../modules/cars/repositories/ISpecificationsRepository';
-import { ICarsRepository } from '@modules/cars/repositories/ICarsRepository';
-import { CarsRepository } from '@modules/cars/infra/typeorm/repositories/CarsRepository';
-
+import { SpecificationsRepository } from "../../modules/cars/infra/typeorm/repositories/SpecificiationsRepository";
+import { ISpecificationsRepository } from "../../modules/cars/repositories/ISpecificationsRepository";
+import { ICarsRepository } from "@modules/cars/repositories/ICarsRepository";
+import { CarsRepository } from "@modules/cars/infra/typeorm/repositories/CarsRepository";
+import { ICarsImagesRepository } from "@modules/cars/repositories/ICarsImagesRepository";
+import { CarsImagesRepository } from "@modules/cars/infra/typeorm/repositories/CarsImagesRepository";
 
 container.registerSingleton<ICategoriesRepository>(
-    "CategoriesRepository",
-    CategoriesRepository
+  "CategoriesRepository",
+  CategoriesRepository
 );
 
 container.registerSingleton<ISpecificationsRepository>(
-    "SpecificationsRepository",
-    SpecificationsRepository
+  "SpecificationsRepository",
+  SpecificationsRepository
 );
 
 container.registerSingleton<IUsersRepository>(
-    "UsersRepository",
-    UsersRepository
+  "UsersRepository",
+  UsersRepository
 );
 
 container.registerSingleton<ICarsRepository>("CarsRepository", CarsRepository);
+
+container.registerSingleton<ICarsImagesRepository>(
+  "CarsImagesRepository",
+  CarsImagesRepository
+);
--- chapter-4/src/shared/infra/http/routes/cars.routes.ts
+++ chapter-4/src/shared/infra/http/routes/cars.routes.ts
@@ -1,9 +1,12 @@
 import { CreateCarController } from "@modules/cars/useCases/createCar/CreateCarController";
 import { CreateCarSpecificationController } from "@modules/cars/useCases/createCarSpecification/CreateCarSpecificationController";
 import { ListAvailableCarsController } from "@modules/cars/useCases/ListAvailableCars/ListAvailableCarsController";
+import { UploadCarImageController } from "@modules/cars/useCases/UploadCarImage/UploadCarImageController";
 import { Router } from "express";
+import uploadConfig from "@config/upload";
 import { ensureAdmin } from "../middlewares/ensureAdmin";
 import { ensureAuthenticated } from "../middlewares/ensureAuthenticated";
+import multer from "multer";
 
 const carsRoutes = Router();
 
@@ -11,6 +14,9 @@ const createCarController = new CreateCarController();
 const listAvailableCarsController = new ListAvailableCarsController();
 const createCarsSpecificationController =
   new CreateCarSpecificationController();
+const uploadCarImagesController = new UploadCarImageController();
+
+const upload = multer(uploadConfig.upload("./tmp/cars"));
 
 carsRoutes.post(
   "/",
@@ -26,6 +32,14 @@ carsRoutes.post(
   ensureAuthenticated,
   ensureAdmin,
   createCarsSpecificationController.handle
+);
+
+carsRoutes.post(
+  "/images",
+  ensureAuthenticated,
+  ensureAdmin,
+  upload.array("images"),
+  uploadCarImagesController.handle
 );
 
 export { carsRoutes };
