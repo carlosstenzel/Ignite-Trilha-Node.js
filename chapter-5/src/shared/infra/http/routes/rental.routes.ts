99472a6f14f33f39b6911c8b116e4e45fbf90517
Carlos Stenzel <carlosstenzel@hotmail.com>
2021-08-10 20:18:57 -0300

    Refatorando a listagem de aluguel do usuario

--- chapter-5/src/shared/infra/http/routes/rental.routes.ts
+++ chapter-5/src/shared/infra/http/routes/rental.routes.ts
@@ -1,5 +1,6 @@
 import { CreateRentalController } from '@modules/rentais/useCases/createRental/CreateRentalController';
 import { DevolutionRentalController } from '@modules/rentais/useCases/devolutionRental/DevolutionRentalController';
+import { ListRentalsByUserController } from '@modules/rentais/useCases/listRentalsByUser/ListRentalsByUserController';
 import { Router } from 'express';
 import { ensureAuthenticated } from '../middlewares/ensureAuthenticated';
 
@@ -7,8 +8,11 @@ const rentalRoutes = Router();
 
 const createRentalController = new CreateRentalController();
 const devolutionRentalController = new DevolutionRentalController();
+const listRentalsByUserController = new ListRentalsByUserController();
 
 rentalRoutes.post("/", ensureAuthenticated , createRentalController.handle);
 rentalRoutes.post("/devolution/:id", ensureAuthenticated , devolutionRentalController.handle);
+
+rentalRoutes.get("user", ensureAuthenticated, listRentalsByUserController.handle);
 
 export {rentalRoutes};
