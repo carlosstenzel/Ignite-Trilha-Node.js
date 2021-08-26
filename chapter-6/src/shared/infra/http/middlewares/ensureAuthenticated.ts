e791a43486aed0d622696cd14828b73d060118a8
Carlos Stenzel <carlosstenzel@hotmail.com>
2021-08-25 21:22:19 -0300

    corrigindo o refresh token

--- chapter-6/src/shared/infra/http/middlewares/ensureAuthenticated.ts
+++ chapter-6/src/shared/infra/http/middlewares/ensureAuthenticated.ts
@@ -13,8 +13,7 @@ interface IPayload {
 export async function ensureAuthenticated(request: Request, response: Response, next: NextFunction) {
 
     const authHeader = request.headers.authorization;
-    const userTokensRepository = new UsersTokensRepository();
-
+    
     if(!authHeader) {
         throw new AppError('Token missing', 401);
     }
@@ -22,13 +21,7 @@ export async function ensureAuthenticated(request: Request, response: Response, 
     const [,token] = authHeader.split("");
 
     try {
-        const { sub: user_id } =  verify(token, auth.secrect_refresh_token) as IPayload;
-
-        const user = await userTokensRepository.findByUserIdAndRefreshToken(user_id, token);
-
-        if(!user){
-            throw new AppError("User does not exists!", 401);
-        }
+        const { sub: user_id } =  verify(token, auth.secrect_token) as IPayload;
 
         request.user = {
             id: user_id
